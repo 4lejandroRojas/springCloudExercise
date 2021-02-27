81a004d48fd68961a1a9941c847fec8ccc221d5a
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2021-02-26 21:46:14 -0600

    Video 31. Return Custom Message Object.

--- src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +10,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(value = {Exception.class})
 	public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+		
+		//Validate localizedMessage which can be null.
+		String errorMessageLocalized = ex.getLocalizedMessage() == null? ex.toString(): ex.getLocalizedMessage();
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageLocalized);
+		
 		return new ResponseEntity<>(
-				ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+				errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
