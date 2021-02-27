aa850a2ebe18bb033745a9612f41bd9b71cfc4e2
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2021-02-27 17:59:46 -0600

    Video 36. Constructor Based Dependency Injection.

--- src/main/java/com/appsdeveloperblog/app/ws/userservice/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/userservice/impl/UserServiceImpl.java
@@ -2,10 +2,11 @@ package com.appsdeveloperblog.app.ws.userservice.impl;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 import com.appsdeveloperblog.app.ws.userservice.UserService;
@@ -15,6 +16,16 @@ public class UserServiceImpl implements UserService{
 
 	Map<String, UserRest> users;
 	
+	
+	Utils utils;
+	
+	public UserServiceImpl() { }
+	
+	@Autowired
+	public UserServiceImpl(Utils utils) {
+		this.utils = utils;
+	}
+	
 	@Override
 	public UserRest createUser(UserDetailsRequestModel userDetails) {
 		UserRest user = new UserRest();
@@ -22,7 +33,7 @@ public class UserServiceImpl implements UserService{
 		user.setFirstName(userDetails.getFirstName());
 		user.setLastName(userDetails.getLastName());
 		
-		String userId = UUID.randomUUID().toString();
+		String userId = utils.generateUserId();
 		user.setUserId(userId);
 		
 		if(users == null) users = new HashMap<>();
